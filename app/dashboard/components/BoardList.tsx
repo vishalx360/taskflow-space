7541a2a8c1ba8d85bfffbb61d497facc77521431
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2023-03-10 14:54:49 +0530

    design update

--- app/dashboard/components/BoardList.tsx
+++ app/dashboard/components/BoardList.tsx
@@ -1,16 +1,23 @@
+import Image from "next/image";
 import { prisma } from "~/server/db";
+import geopattern from "geopattern";
 
 export default async function BoardList({ workspaceId }: { workspaceId: string }) {
     const boards = await prisma.board.findMany({ where: { workspaceId } });
+    const background = geopattern.generate(workspaceId).toDataUri()
     return (
-        <div className="flex items-center p-10 gap-10 ">
+        <div className="flex items-center gap-10 ">
             {boards.map(board => {
-                return <div key={board.id} className="py-5 px-10 bg-gray-300 rounded-xl">
-                    <div className='p-2'>
+                return <div key={board.id} className={` bg-red-300 relative overflow-hidden rounded-xl`}>
+                    <Image
+                        height="50"
+                        width="150"
+                        src={background}
+                        alt=""
+                        className="object-cover h-32 w-52"
+                    />
+                    <div className='text-white font-bold absolute bottom-0 p-5'>
                         {board.name}
-                        <p>
-                            Created on: {board.createdAt.toLocaleDateString()}
-                        </p>
                     </div>
                 </div>
             })}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,19 +4,15 @@ import BoardList from "./components/BoardList";
 async function DashboardPage() {
     const workspaces = await prisma.workspace.findMany();
     return (
-        <div className="p-10">
+        <div className="p-10 space-y-10">
             {/* TOOD: show workspaces tabs  */}
             {workspaces?.map((workspace) => {
                 return (
-                    <div key={workspace.id} className="border-2 border-gray-300">
-                        <div className='p-2'>
+                    <div key={workspace.id} className="">
+                        <div className='p-2 border-b-2 border-gray-300 my-5 text-xl'>
                             {workspace.name}
-                            <p>
-                                Created on: {workspace.createdAt.toLocaleDateString()}
-                            </p>
                         </div>
-                        <div className='p-2'>
-                            Boards
+                        <div className="">
                             <BoardList workspaceId={workspace.id} />
                         </div>
                     </div>
