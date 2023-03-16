3a472f08ee2b6379c656f92772c7c669f672d9ec
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2023-03-16 12:43:28 +0530

    add disclosoure to workspace in dashboard

--- src/modules/Dashboard/Workspaces.tsx
+++ src/modules/Dashboard/Workspaces.tsx
@@ -1,3 +1,5 @@
+import { Disclosure, Transition } from "@headlessui/react";
+import { FaCaretRight } from "react-icons/fa";
 import BoardList, { BoardListSkeleton } from "~/modules/Dashboard/BoardList";
 import { api } from "~/utils/api";
 
@@ -7,23 +9,56 @@ function Workspaces() {
         return <WorkspaceListSkeleton />
     }
     return (
-        <div className="space-y-10">
-            <h1 className="uppercase tracking-wider">Your Workspaces</h1>
-            {/* TOOD: show workspaces tabs  */}
-            {workspaces?.map((workspace) => {
-                return (
-                    <div key={workspace.id} className="">
-                        <div className='p-2 border-b-2 border-gray-200 font-semibold my-5 text-xl'>
-                            {workspace.name}
-                        </div>
-                        <div className="">
-                            <BoardList workspaceId={workspace.id} />
-                        </div>
-                    </div>
-                )
+        <div>
+            <h1 className="uppercase tracking-wider text-neutral-500 font-medium">Your Workspaces</h1>
+            <div className="space-y-10">
+                {/* TOOD: show workspaces tabs  */}
+                {workspaces?.map((workspace) => {
+                    return (
+                        <div key={workspace.id} className="">
+                            <Disclosure defaultOpen>
+                                {({ open }) => (
+                                    <>
+                                        <Disclosure.Button className="w-full transition-all">
+                                            <div className='w-full flex items-center justify-between gap-10 p-2 border-b-2 border-gray-200 font-semibold my-5 text-xl'>
+                                                <h1>
+                                                    {workspace.name}
+                                                </h1>
+                                                {/* <IconButton Icon={MdSettings} >
+                                                Settings
+                                            </IconButton> */}
 
-            })}
-            {workspaces?.length === 0 && "No Workspace Found"}
+                                                <FaCaretRight
+                                                    className={`${open ? "rotate-90 transform" : ""
+                                                        } h-5 w-5 text-inherit`}
+                                                />
+                                            </div>
+
+                                        </Disclosure.Button>
+                                        <Transition
+                                            enter="transition duration-150 ease-in"
+
+                                            enterFrom="transform  -translate-y-3 opacity-0"
+                                            enterTo="transform translate-y-0  opacity-100"
+
+                                            leave="transition duration-150 ease-out"
+
+                                            leaveFrom="transform translate-y-0 opacity-100"
+                                            leaveTo="transform -translate-y-3 opacity-0"
+                                        >
+                                            <Disclosure.Panel>
+                                                <BoardList workspaceId={workspace.id} />
+                                            </Disclosure.Panel>
+                                        </Transition>
+                                    </>
+                                )}
+                            </Disclosure>
+                        </div>
+                    )
+
+                })}
+                {workspaces?.length === 0 && "No Workspace Found"}
+            </div>
         </div>
     )
 }
