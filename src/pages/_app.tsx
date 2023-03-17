e45843b02935918a9abfdc0c9ee2927f47c428f5
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2023-03-17 19:48:58 +0530

    remove custom font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,21 +7,14 @@ import { api } from "~/utils/api";
 import { Toaster } from "react-hot-toast";
 import "~/styles/globals.css";
 
-import { Raleway } from "next/font/google";
-
-// If loading a variable font, you don't need to specify the font weight
-const raleway = Raleway({ subsets: ["latin"] });
-
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
-      <main className={raleway.className}>
-        <Toaster position="bottom-center" />
-        <Component {...pageProps} />
-      </main>
+      <Toaster position="bottom-center" />
+      <Component {...pageProps} />
     </SessionProvider>
   );
 };
