580d310d359492e998c91eb703aa62b7aad3dfeb
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2023-03-27 19:55:00 +0530

    add accountmenu in homepage navbar

--- src/modules/Global/LogoImage.tsx
+++ src/modules/Global/LogoImage.tsx
@@ -2,14 +2,20 @@ import Image from "next/image";
 import LOGO_URL from "../../../public/logo/taskflow-full-logo.png";
 import BLACK_LOGO_URL from "../../../public/logo/taskflow-full-logo-black.png";
 
-function LogoImage({ dark = false }: { dark?: boolean }) {
+function LogoImage({
+  className,
+  dark = false,
+}: {
+  className?: string;
+  dark?: boolean;
+}) {
   return (
     <Image
       width="250"
       height="50"
       src={dark ? BLACK_LOGO_URL : LOGO_URL}
       placeholder="blur"
-      className="mr-3 "
+      className={className}
       alt="Taskflow.space Logo"
       quality={100}
     />
--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -1,10 +1,18 @@
-import { Transition } from "@headlessui/react";
+import { Menu, Transition } from "@headlessui/react";
+import { type Session } from "@prisma/client";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
-
+import { Fragment, memo, useState } from "react";
+import { FiChevronDown, FiLogOut } from "react-icons/fi";
+import getGravatar from "~/utils/getGravatar";
 import POSTER_URL from "../../../public/poster.png";
 import LogoImage from "../Global/LogoImage";
+
+async function handelLogout() {
+  await signOut();
+  return;
+}
 
 const NavLinks = [
   { title: "Home", href: "/home" },
@@ -13,7 +21,10 @@ const NavLinks = [
 ];
 
 function Home() {
+  const { data: session, status } = useSession();
+
   const [isOpen, setIsOpen] = useState(false);
+
   function toggel() {
     setIsOpen((prev) => !prev);
   }
@@ -23,53 +34,74 @@ function Home() {
         <nav className="container mx-auto p-6 lg:flex lg:items-center lg:justify-between">
           <div className="flex items-center justify-between">
             <Link href="/" className="flex items-center">
-              <LogoImage dark />
+              <LogoImage className="w-52 lg:w-full" dark />
             </Link>
 
-            {/* <!-- Mobile menu button --> */}
+            {/* <!-- Mobile Navbar button --> */}
             <div className="flex lg:hidden">
-              <button
-                onClick={toggel}
-                type="button"
-                className="text-gray-500 hover:text-gray-600 focus:text-gray-600 focus:outline-none "
-                aria-label="toggle menu"
-              >
-                {!isOpen ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4 8h16M4 16h16"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                )}
-              </button>
+              {status === "authenticated" ? (
+                <button onClick={toggel} type="button" aria-label="toggle menu">
+                  <div className="group inline-flex w-full items-center justify-center gap-2 rounded-md md:gap-5">
+                    {session?.user?.email && (
+                      <Image
+                        height={200}
+                        width={200}
+                        // generate default gravtar image
+                        src={
+                          session?.user?.image ||
+                          getGravatar(session?.user?.email)
+                        }
+                        alt="avatar"
+                        className="w-10 rounded-full ring-2 ring-white/40 transition-all group-hover:ring-4"
+                      />
+                    )}
+                    <FiChevronDown className="text-xl" />
+                  </div>
+                </button>
+              ) : (
+                <button
+                  onClick={toggel}
+                  type="button"
+                  aria-label="toggle menu"
+                  className="text-gray-500 hover:text-gray-600 focus:text-gray-600 focus:outline-none "
+                >
+                  {!isOpen ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4 8h16M4 16h16"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  )}
+                </button>
+              )}
             </div>
           </div>
 
-          {/* <!-- Mobile Menu open: "block", Menu closed: "hidden" --> */}
+          {/* <!-- Mobile Navbar --> */}
           <div className="block lg:hidden">
             <Transition
               show={isOpen}
@@ -82,15 +114,33 @@ function Home() {
               className="absolute inset-x-0 top-20 z-20 block w-full rounded-b-xl bg-neutral-100 px-6 py-4 shadow-xl transition-all duration-300 ease-in-out"
             >
               <div className="flex flex-col space-y-5">
+                <div className=" mb-2 text-left">
+                  <h1 className="mt-3 text-sm font-medium">
+                    {session?.user?.name}
+                  </h1>
+                  <h1 className="text-xs font-medium text-black text-opacity-50">
+                    {session?.user.email}
+                  </h1>
+                </div>
+
                 {NavLinks.map((navlink) => (
                   <Link
                     key={navlink.href}
-                    className="transform text-gray-700 transition-colors duration-300 hover:text-green-500 lg:mx-8"
+                    className="transform text-gray-700 transition-colors duration-300  lg:mx-8"
                     href={navlink.href}
                   >
                     {navlink.title}
                   </Link>
                 ))}
+
+                <button
+                  onClick={() => {
+                    void handelLogout();
+                  }}
+                  className="text-left text-gray-700 transition-colors duration-300  lg:mx-8"
+                >
+                  Logout
+                </button>
               </div>
 
               {/* <a
@@ -113,6 +163,9 @@ function Home() {
                     {navlink.title}
                   </Link>
                 ))}
+                {status === "authenticated" && (
+                  <AccountMenuMemo session={session} />
+                )}
               </div>
             </div>
           </div>
@@ -152,3 +205,63 @@ function Home() {
 }
 
 export default Home;
+
+function AccountMenu({ session }: { session: Session | null }) {
+  return (
+    <div>
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="group inline-flex w-full items-center justify-center gap-2 rounded-md md:gap-5">
+            {session?.user?.email && (
+              <Image
+                height={200}
+                width={200}
+                // generate default gravtar image
+                src={session?.user?.image || getGravatar(session?.user?.email)}
+                alt="avatar"
+                className="w-10 rounded-full ring-2 ring-white/40 transition-all group-hover:ring-4"
+              />
+            )}
+            <FiChevronDown className="text-xl" />
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-20 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white text-black shadow-xl ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="px-5 py-3">
+              <div className=" mb-2 text-left">
+                <h1 className="mt-3 text-sm font-medium">
+                  {session?.user?.name}
+                </h1>
+                <h1 className="text-xs font-medium text-black text-opacity-50">
+                  {session?.user.email}
+                </h1>
+              </div>
+
+              <Menu.Item>
+                <button
+                  className={`group flex w-full items-center rounded-md px-3 py-3 text-sm hover:bg-red-500 hover:text-white`}
+                  onClick={() => {
+                    void handelLogout();
+                  }}
+                >
+                  <FiLogOut className="mr-2 h-5 w-5" aria-hidden="true" />
+                  Logout
+                </button>
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  );
+}
+
+const AccountMenuMemo = memo(AccountMenu);
