678b856db4e8264f619d284cc541cbf51704e474
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2023-04-26 16:57:11 +0530

    make trpc work offline

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -28,6 +28,7 @@ export const api = createTRPCNext<AppRouter>({
             refetchOnWindowFocus: false,
             retry: false,
             staleTime: 1000 * 60 * 1, // 1 minutes
+            networkMode: "always"
           },
         },
       },
