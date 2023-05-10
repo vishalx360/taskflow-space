8e9c7ed5729e690f6e5933c53bebdf7b7eb4b91f
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2023-05-10 15:11:03 +0530

    add motion to signin and signup pages

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -16,6 +16,7 @@ import { useCallback, useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { toFormikValidationSchema } from "zod-formik-adapter";
+import { motion } from "framer-motion";
 
 export default function SignInPage() {
   const router = useRouter();
@@ -58,7 +59,12 @@ export default function SignInPage() {
       </Head>
       <section className="bg-neutral-100 dark:bg-neutral-900">
         <div className="mx-auto flex h-screen flex-col items-center justify-center px-6 py-8 lg:py-0">
-          <div className="flex items-center  gap-5">
+          <motion.div
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "anticipate" }}
+            className="flex items-center  gap-5"
+          >
             <Link
               href="/"
               className="flex items-center gap-5 rounded-full border-neutral-400 p-2 transition duration-200 ease-in-out hover:bg-neutral-500/20 "
@@ -71,8 +77,13 @@ export default function SignInPage() {
             <div className="my-10 hidden dark:block lg:my-20">
               <LogoImage width={300} />
             </div>
-          </div>
-          <div className="w-full rounded-xl bg-white shadow-lg dark:border dark:border-neutral-700 dark:bg-neutral-800 sm:max-w-md md:mt-0 xl:p-0">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "anticipate" }}
+            className="w-full rounded-xl bg-white shadow-lg dark:border dark:border-neutral-700 dark:bg-neutral-800 sm:max-w-md md:mt-0 xl:p-0"
+          >
             <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
               <h1 className="text-xl font-medium leading-tight tracking-tight text-neutral-900 dark:text-white md:text-2xl">
                 Sign in to your account
@@ -183,7 +194,7 @@ export default function SignInPage() {
                 </Link>
               </p>
             </div>
-          </div>
+          </motion.div>
         </div>
       </section>
     </>
