8679b0bcadc4ad3c2f3795fa1aea7b46c2b56cb0
vishalx360 <25267239+vishalx360@users.noreply.github.com>
2023-07-26 21:13:53 +0530

    remove image , add icons

--- src/modules/Home/BenifitsSection.tsx
+++ src/modules/Home/BenifitsSection.tsx
@@ -1,29 +1,25 @@
-import Image from "next/image";
 
-import BENIFIT_2_IMG from "../../../public/benifit_2.png";
-import BENIFIT_3_IMG from "../../../public/benifit_3.png";
-import BENIFIT_1_IMG from "../../../public/passkey.png";
+import { LucideBell, LucideLock, LucideRefreshCw } from "lucide-react";
+import Link from "next/link";
 
 const benifits = [
   {
-    poster: "#",
-    image: BENIFIT_1_IMG,
+    icon: LucideLock,
     name: "Passkey Support",
     title: "Authentication Made Easy",
     description:
       "Enjoy a secure and hassle-free login process with passkey support, ensuring your data stays protected at all times.",
+    learnMoreLink: "https://developer.apple.com/passkeys",
   },
   {
-    poster: "#",
-    image: BENIFIT_2_IMG,
+    icon: LucideBell,
     name: "Email Alerts",
     title: "Stay Updated, Stay on Track",
     description:
       "Receive timely email alerts and notifications, keeping you informed about task updates and deadlines, so you never miss a beat.",
   },
   {
-    poster: "#",
-    image: BENIFIT_3_IMG,
+    icon: LucideRefreshCw,
     name: "Real-time Updates",
     title: "Stay in Sync, Always",
     description:
@@ -34,7 +30,7 @@ function BenifitsSection() {
   return (
     <section
       id="benifits"
-      className="min-h-screen scroll-mt-16 bg-gradient-to-b from-black/10 to-white "
+      className=" scroll-mt-16 bg-gradient-to-b from-black/10 to-white "
     >
       <div className="container mx-auto px-6 py-16 ">
         <div className="space-y-4">
@@ -57,7 +53,7 @@ function BenifitsSection() {
           {benifits.map((benifit) => {
             return (
               <div key={benifit.title} className="space-y-10">
-                <div className="group row-span-3 aspect-video overflow-hidden rounded-md bg-neutral-200 md:aspect-square lg:row-span-5 ">
+                {/* <div className="group row-span-3 aspect-video overflow-hidden rounded-md bg-neutral-200 md:aspect-square lg:row-span-5 ">
                   <Image
                     alt="Poster"
                     width={800}
@@ -66,13 +62,20 @@ function BenifitsSection() {
                     className="h-full w-full rounded-xl border object-cover shadow-2xl transition-all duration-700  md:translate-x-14  md:translate-y-14 md:group-hover:translate-x-12 md:group-hover:translate-y-12"
                     src={benifit.image}
                   />
-                </div>
+                </div> */}
                 <div className="row-span-2 space-y-2 p-1 text-left lg:row-span-1">
-                  <h1 className="text-xl font-medium text-blue-600">
-                    {benifit.name}
-                  </h1>
+                  <div className="mb-5 flex items-center text-blue-600 gap-2">
+                    <benifit.icon size={30} />
+                    <h1 className="text-xl font-semibold ">
+                      {benifit.name}
+                    </h1>
+                  </div>
                   <h1 className="text-2xl font-medium">{benifit.title}</h1>
-                  <p>{benifit.description}</p>
+                  <p>{benifit.description}
+                    {benifit.learnMoreLink && (
+                      <Link className="underline underline-offset-2 ml-1" href={benifit.learnMoreLink}>Learn More about passkeys</Link>)
+                    }
+                  </p>
                 </div>
               </div>
             );
