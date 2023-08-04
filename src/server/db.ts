6bb573ebeef5b0dd93d080c979c2536ddf58b5c3
vishalx360 <25267239+vishalx360@users.noreply.github.com>
2023-08-04 17:12:14 +0530

    add prisma-redis-middleware

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,6 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { createPrismaRedisCache } from "prisma-redis-middleware";
+import superjson from "superjson";
 
 import { env } from "@/env.mjs";
+import { redisClient } from "@/lib/redisClient";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -12,5 +15,28 @@ export const prisma =
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
+
+
+
+const cacheMiddleware: Prisma.Middleware = createPrismaRedisCache({
+  models: [],
+  storage: { type: "redis", options: { client: redisClient, invalidation: { referencesTTL: 300 }, } },
+  cacheTime: 500,
+  transformer: {
+    serialize: (result) => superjson.serialize(result),
+    deserialize: (serialized) => superjson.deserialize(serialized),
+  },
+  onHit: (key) => {
+    console.log("hit", key);
+  },
+  onMiss: (key) => {
+    console.log("miss", key);
+  },
+  onError: (key) => {
+    console.log("error", key);
+  },
+});
+
+prisma.$use(cacheMiddleware);
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
